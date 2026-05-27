83d033f85f00f437ecd50979152b2b99cdc2eeaf
Yash salunke <codingwithcandy@gmail.com>
2026-05-27 15:44:08 +0530

    url add in origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173', 'https://auth-mern-javascript.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://auth-mern-javascript.vercel.app',
+    'https://auth-mern-javascript-git-master-yash-8888s-projects.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
